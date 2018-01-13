21c275d080c46e89a26a83320e3a6a85060d5b98
User <User@Usuario>
2018-01-13 13:19:27 +0100

    Inicio

---
+++ Documents/2_PROG/Arch NetBeansProjects/Supuesto_3/src/supuesto_3/Supuesto_3.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package supuesto_3;
+
+/**
+ *
+ * @author User
+ */
+import java.util.Scanner;
+public class Supuesto_3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    Scanner teclado = new Scanner (System.in);                         
+        int numero;
+        int i;       
+        System.out.println("Introducir un número: " );
+            numero = teclado.nextInt();
+                for(i=2;i<=numero;i++)  {
+
+                while(numero%i==0){
+                numero=numero/i;
+                System.out.print(i+" , ");
+              
+                }
+            }
+        }
+    }             
